85bc3c1e5712aea6c40f59b25244661b0905c8a3
vitor <vitor@localhost.localdomain>
2013-06-15 22:52:47 -0300

    Header para Instalar os Pacotes

---
+++ htlp_install.hpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   htlp_install.hpp
+ * Author: vitor
+ *
+ * Created on June 15, 2013, 10:29 PM
+ */
+
+#ifndef HTLP_INSTALL_HPP
+#define	HTLP_INSTALL_HPP
+
+class HTLP_Install {
+    
+};
+
+
+#endif	/* HTLP_INSTALL_HPP */
+
