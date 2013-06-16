8b3d1780f531b92e7cf9553221260105be46f716
vitor <vitor@localhost.localdomain>
2013-06-15 22:52:28 -0300

    Header para Download de Pacotes

---
+++ htlp_download.hpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   htlp_download.hpp
+ * Author: vitor
+ *
+ * Created on June 15, 2013, 10:52 PM
+ */
+
+#ifndef HTLP_DOWNLOAD_HPP
+#define	HTLP_DOWNLOAD_HPP
+
+
+
+#endif	/* HTLP_DOWNLOAD_HPP */
+
