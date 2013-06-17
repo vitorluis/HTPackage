5624ec39d2976a1f1bde143b143404f04ee93d08
vitor <vitor@localhost.localdomain>
2013-06-17 20:39:51 -0300

    Header com a licença GNU

--- htlp_install.hpp
+++ htlp_install.hpp
@@ -3,12 +3,12 @@
  * htlp_install.hpp
  * Copyright (C) 2013 Vitor Luis V. Neto <vitor@softwaredevelopment.com.br>
  * 
- * VBackup is free software: you can redistribute it and/or modify it
+ * HTPackage is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * VBackup is distributed in the hope that it will be useful, but
+ * HTPackage is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
