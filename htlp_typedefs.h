3fa1ab7bb80910da245165fa0175e3ed6e9b2174
vitor <vitor@localhost.localdomain>
2013-06-22 22:56:44 -0300

    Header com os typedefs do sistema

---
+++ htlp_typedefs.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   htlp_typedefs.h
+ * Author: vitor
+ *
+ * Created on June 22, 2013, 10:54 PM
+ */
+
+#ifndef HTLP_TYPEDEFS_H
+#define	HTLP_TYPEDEFS_H
+
+//Struct Package
+typedef struct package Package;
+
+//Struct Databasse
+typedef struct database Database;
+
+//Struct Data Connection
+typedef struct data_connection Data_Connection;
+
+#endif	/* HTLP_TYPEDEFS_H */
+
