4a0a390233e6e7a4ad41fba05d8c94d907b47bfe
vitor <vitor@localhost.localdomain>
2013-06-26 20:52:59 -0300

    Header com as funções de parse do arquivo de configuração

--- htlp_parse_conf.h
+++ htlp_parse_conf.h
@@ -7,7 +7,16 @@
 
 #ifndef HTLP_PARSE_CONF_H
 #define	HTLP_PARSE_CONF_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <libconfig.h>
 
+int htlp_parse_conf_main();
 
+int htlp_parse_conf_open_file();
 #endif	/* HTLP_PARSE_CONF_H */
 
